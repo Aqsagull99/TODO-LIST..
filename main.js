63912a88bebed879b7347d31d30ee3731e157437
Aqsa Gull <aqsabravegirl3@gmail.com>
2024-03-30 11:44:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// import inquirer from "inquirer"
+// let todos = [];
+// let condition = "true";
+// while (condition) {
+//   let addTask = await inquirer.prompt([
+//     {
+//       name: "todo",
+//       type: "input",
+//       message:"what do you want to add in your todos?",
+//     },
+//     {
+//       name: "addmore",
+//       type: "confirm",
+//       message: "Is it confirm do you want to add more?",
+//       default: "false"
+//     }
+//   ]);
+//   todos.push(addTask.todo);
+//   condition = addTask.addmore;
+//   console.log(todos);
+// }
+import inquirer from "inquirer";
+let todos = [];
+let condition = true; // Initialize as boolean true
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what do you want to add in your todos?",
+        },
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Is it confirm do you want to add more?",
+            default: false // Initialize as boolean false
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addmore;
+    console.log(todos);
+}
